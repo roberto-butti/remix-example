02c7d4880cb7d06bf0e25d741f323c95c2178112
Roberto B <roberto.butti@gmail.com>
2022-02-18 23:24:01 +0100

    Refactor index.jsx, using CallToAction component

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,30 +1,24 @@
+import CallToAction from '../components/CallToAction'
+
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
+          <CallToAction
+            url="https://remix.run/tutorials/blog"
+            title="15m Quickstart Blog Tutorial" />
         </li>
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
+          <CallToAction
+            url="https://remix.run/tutorials/jokes"
+            title="Deep Dive Jokes App Tutorial" />
         </li>
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
+          <CallToAction
+            url="https://remix.run/docs"
+            title="Remix Docs" />
         </li>
       </ul>
     </div>
