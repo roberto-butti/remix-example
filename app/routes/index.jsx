514bfb5ef603ac00920aa1b6ed96090d5ee9fbed
Roberto B <roberto.butti@gmail.com>
2022-02-28 08:46:52 +0100

    load content from JSON

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,26 +1,38 @@
 import CallToAction from '../components/CallToAction'
+import { useLoaderData } from "remix";
+
+export const loader = async () => {
+  return [
+    {
+      url: "https://remix.run/tutorials/blog",
+      title: "15m Quickstart Blog Tutorial"
+    },
+    {
+      url: "https://remix.run/tutorials/jokes",
+      title: "Deep Dive Jokes App Tutorial"
+    },
+    {
+      url: "https://remix.run/docs",
+      title: "Remix Docs"
+    }
+  ]
+}
 
 export default function Index() {
+  const urls = useLoaderData()
+  console.log(urls)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
       <ul>
-        <li>
-          <CallToAction
-            url="https://remix.run/tutorials/blog"
-            title="15m Quickstart Blog Tutorial" />
-        </li>
-        <li>
-          <CallToAction
-            url="https://remix.run/tutorials/jokes"
-            title="Deep Dive Jokes App Tutorial" />
-        </li>
-        <li>
-          <CallToAction
-            url="https://remix.run/docs"
-            title="Remix Docs" />
-        </li>
+        {urls.map(url => (
+          <li key={url.url}>
+            <CallToAction
+              {...url}
+            />
+          </li>
+        ))}
       </ul>
     </div>
-  );
+  )
 }
